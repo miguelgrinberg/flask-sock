00f3e58538e4f86112c7916a0fc7827b45608860
Gilbert Brault <gilbert.brault@hotmail.com>
2023-02-03 07:51:28 +0100

    Adding a clock to the ws example
    The original example was presenting how to communicate over ws to a flask server.
    But when the ws chanel is opened, it can be used both way.
    So added a small clock thread to show that

--- examples/echo.py
+++ examples/echo.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from flask_sock import Sock
+import json
+import time, datetime
+import threading
 
 app = Flask(__name__)
 app.config['SOCK_SERVER_OPTIONS'] = {'ping_interval': 25}
@@ -11,14 +14,23 @@ sock = Sock(app)
 def index():
     return render_template('index.html')
 
+def send_time(ws):
+    # simple loop spiting server time into the ws chanel as a json structure
+    while True:
+        time.sleep(1)
+        ws.send(json.dumps({"type": "clock", "text": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}))
 
 @sock.route('/echo')
 def echo(ws):
+    # start a thread to activate the clock
+    t = threading.Thread(target=send_time,args=(ws,))
+    t.start()
     while True:
         data = ws.receive()
         if data == 'close':
             break
-        ws.send(data)
+        # note that the data is now (from original code) an object to provide the type selector
+        ws.send(json.dumps({"type": "log", "text": data}))
 
 
 if __name__ == '__main__':
